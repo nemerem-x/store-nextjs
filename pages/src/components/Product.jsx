deb2a49507a561839aec462ca3d7238fbffc11a8
nemerem-x <pilotprecious@gmail.com>
2022-12-24 22:57:41 +0100

    products

---
+++ pages/src/components/Product.jsx
@@ -0,0 +1,23 @@
+import Image from "next/image"
+import styles from '../../../styles/Product.module.css'
+
+export default function Product({product}) {
+
+    return (
+    <div className={styles.product}>
+        <div className="productImage">
+            <Image src={'/img.jpeg'} width='200' height={'200'} alt='product' />
+        </div>
+
+        <div className={styles.title}>
+            <h2>{product.title}</h2>
+            <p>{product.rating.rate}</p>
+
+            <div className={styles.price}>
+                <p>$</p>
+                <p>{product.price}</p>
+            </div>
+        </div>
+    </div>
+  )
+}
