cca67a93aba484d8cefbdaca76d456bea29d766f
nemerem-x <pilotprecious@gmail.com>
2022-12-25 23:39:14 +0100

    next/link

--- pages/src/components/Product.jsx
+++ pages/src/components/Product.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
 import styles from '../../../styles/Product.module.css'
-import Link from 'next/Link'
+import Link from 'next/link'
 
 export default function Product({product}) {
 
