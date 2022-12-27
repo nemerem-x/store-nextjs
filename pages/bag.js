bc4d250b80e8ae179f25373c0a62304380404e1e
nemerem-x <pilotprecious@gmail.com>
2022-12-27 20:24:22 +0100

    bag

--- pages/bag.js
+++ pages/bag.js
@@ -4,22 +4,163 @@ export default function bag() {
     
   return (
     <div className={styles.bag}>
-      <div className={styles.bagitems}>
         <h2>Shopping Bag</h2>
 
-        <div className={styles.item}>
-          <img></img>
-          <p>Item 1 - school bag</p>
-          <div className={styles.price}>
-                <p>$</p>
-                <p>100</p>
-          </div>
+        <div className={styles.baginfo}>
+            <div className={styles.bagitems}>
+
+              <div className={styles.item}>
+                    <img></img>
+                    <div className={styles.info}>
+                      <p className={styles.title}>Item 1 - school bag</p>
+                      <p>category</p>
+
+                      <div className={styles.quantity}>
+                        <p>Quantity</p>
+                        <select>
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                          <option>6</option>
+                          <option>7</option>
+                          <option>8</option>
+                          <option>9</option>
+                          <option>10</option>
+                        </select>
+                      </div>
+                    </div>
+
+                    <div className={styles.price}>
+                          <p>$</p>
+                          <p>100</p>
+                    </div>
+              </div>
+
+              <div className={styles.item}>
+                    <img></img>
+                    <div className={styles.info}>
+                      <p className={styles.title}>Item 1 - school bag</p>
+                      <p>category</p>
+
+                      <div className={styles.quantity}>
+                        <p>Quantity</p>
+                        <select>
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                          <option>6</option>
+                          <option>7</option>
+                          <option>8</option>
+                          <option>9</option>
+                          <option>10</option>
+                        </select>
+                      </div>
+                    </div>
+
+                    <div className={styles.price}>
+                          <p>$</p>
+                          <p>100</p>
+                    </div>
+              </div>
+
+              <div className={styles.item}>
+                    <img></img>
+                    <div className={styles.info}>
+                      <p className={styles.title}>Item 1 - school bag</p>
+                      <p>category</p>
+
+                      <div className={styles.quantity}>
+                        <p>Quantity</p>
+                        <select>
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                          <option>6</option>
+                          <option>7</option>
+                          <option>8</option>
+                          <option>9</option>
+                          <option>10</option>
+                        </select>
+                      </div>
+                    </div>
+
+                    <div className={styles.price}>
+                          <p>$</p>
+                          <p>100</p>
+                    </div>
+              </div>
+
+              <div className={styles.item}>
+                    <img></img>
+                    <div className={styles.info}>
+                      <p className={styles.title}>Item 1 - school bag</p>
+                      <p>category</p>
+
+                      <div className={styles.quantity}>
+                        <p>Quantity</p>
+                        <select>
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                          <option>6</option>
+                          <option>7</option>
+                          <option>8</option>
+                          <option>9</option>
+                          <option>10</option>
+                        </select>
+                      </div>
+                    </div>
+
+                    <div className={styles.price}>
+                          <p>$</p>
+                          <p>100</p>
+                    </div>
+              </div>
+
+              <div className={styles.item}>
+                    <img></img>
+                    <div className={styles.info}>
+                      <p className={styles.title}>Item 1 - school bag</p>
+                      <p>category</p>
+
+                      <div className={styles.quantity}>
+                        <p>Quantity</p>
+                        <select>
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                          <option>6</option>
+                          <option>7</option>
+                          <option>8</option>
+                          <option>9</option>
+                          <option>10</option>
+                        </select>
+                      </div>
+                    </div>
+
+                    <div className={styles.price}>
+                          <p>$</p>
+                          <p>100</p>
+                    </div>
+              </div>
+
+            </div>
+
+            <div className={styles.summary}>
+
+            </div>
         </div>
-      </div>
 
-      <div className={styles.summary}>
-
-      </div>
     </div>
   )
 }
