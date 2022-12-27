d2ce7e8326f2e8c6378a78b2f36baa534a632144
nemerem-x <pilotprecious@gmail.com>
2022-12-27 17:01:19 +0100

    bag page

--- pages/bag.js
+++ pages/bag.js
@@ -1,9 +1,25 @@
+import styles from '../styles/Bag.module.css'
 
 export default function bag() {
     
   return (
-    <div>
-        bag
+    <div className={styles.bag}>
+      <div className={styles.bagitems}>
+        <h2>Shopping Bag</h2>
+
+        <div className={styles.item}>
+          <img></img>
+          <p>Item 1 - school bag</p>
+          <div className={styles.price}>
+                <p>$</p>
+                <p>100</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.summary}>
+
+      </div>
     </div>
   )
 }
